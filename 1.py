c7a353cba438561eef9937771188e8e05b4a6a2e
Sidharth-Hemani <35894006+Sidharth-Hemani@users.noreply.github.com>
2018-01-29 19:37:50 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,16 +1,15 @@
 from urllib.request import urlopen as uopen
-from urllib.request import urlretrieve as img_d
 from bs4 import BeautifulSoup as soup
-def collected_info(url,date):		
-	date=date.replace('/','')
-	print(date)	
+
+def collected_info(url,date):			
+	date=date.replace('/','')	
 	url = url + date+'hl.html'	
 	download_html = uopen(url)
 	page_html = download_html.read()
 	page_soup = soup(page_html,'lxml')
-	news_feed = page_soup.find_all('a',{'target':'_new'})	
-	for x in news_feed:	
-		print(x.text)
+	news_feed = page_soup.find_all('div',{'id':'hdtab1'})		
+	for x in news_feed: 
+		print(x.text.replace('IST',"IST"+'\n'))	
 print('Enter the date for headlines in DD/MM/YY format')
 date = str(input())	
 collected_info('http://www.rediff.com/issues/',date)
